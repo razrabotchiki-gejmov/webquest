fb22526a2ce74cb05b93341b98794a7a9936d46c
sdfdfsff <anton8806@gmail.com>
2024-11-17 23:54:58 +0500

    шаблон
    шаблон

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import * as THREE from 'three'
+import './index.css'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/ window.innerHeight, 0.1,1000)
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
+renderer.render(scene,camera)
+
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347})
+const torus = new THREE.Mesh(geometry,material)
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+
+const ambient = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight,ambient)
+
+const lhelper = new THREE.PointLightHelper(pointLight)
+const gridHealper = new THREE.GridHelper(200,50)
+scene.add(lhelper,gridHealper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+function animate(){
+  requestAnimationFrame(animate)
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+  controls.update()
+  renderer.render(scene,camera)
+}
+animate()
+
+
